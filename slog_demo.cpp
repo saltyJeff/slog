a594eb94e214ec1c6f70b4ac2c1841dc23d0de2e
saltyJeff <saltyJeff@users.noreply.github.com>
2024-09-05 22:26:50 -0700

    add cpp11 and cpp17 demo

--- slog_demo.cpp
+++ slog_demo.cpp
@@ -4,11 +4,13 @@ int main()
 {
     SLOG(ERROR) << "this is an error in stream form";
     SLOG(WARN, "this is a warning in string form");
+    #if SLOG_FMT > 0 // SLOG_FMT will be non-zero if one of the formatting libraries is detected
     SLOG(INFO, "this is an {} in {} form", "info", "format");
+    #endif
     // test this issue: https://stackoverflow.com/questions/5028302/small-logger-class#comment69428442_5028917
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < 16; i++)
     {
-        SLOG(DEBUG, "this is a debug log: {:d}", i);
+        SLOG(DEBUG) << "this is a debug log: " << std::hex << i;
     }
     SLOG_IF(ERROR, false) << "this doesn't exist";
 }
